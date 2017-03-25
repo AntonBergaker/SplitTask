b0bcc2da245609c97467c447623103e965d4649d
Anton <noten.b@hotmail.com>
2017-03-25 11:56:01 +0100

    Renamed to SplitTask

--- Planner/DummyProjectThatBootsManyTaskPlanners/Program.cs
+++ Planner/DummyProjectThatBootsManyTaskPlanners/Program.cs
@@ -12,7 +12,7 @@ namespace DummyProjectThatBootsManyClients
         static void Main(string[] args)
         {
             DirectoryInfo directory = Directory.GetParent(Directory.GetParent(Directory.GetParent(Environment.CurrentDirectory).ToString()).ToString());
-            string path = directory + @"\TaskPlanner\bin\Debug\TaskPlanner.exe";
+            string path = directory + @"\SplitTask\bin\Debug\SplitTask.exe";
             for (int i = 0; i < 2; i++)
             { System.Diagnostics.Process.Start(path); }
         }
